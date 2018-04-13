4e627af2159d76aaa505af2832f8b2d60621ce3d
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-13 15:35:27 +0100

    Incomplete Test

--- lib/foobarqix.rb
+++ lib/foobarqix.rb
@@ -6,7 +6,7 @@ def check_divisors(integer)
   elsif integer % 7 == 0
     "qix"
   else
-    integer
+    ""
   end
 end
 
@@ -19,32 +19,26 @@ def count_digits(integer)
 end
 
 def digit_check(digit_counts)
-
+  if digit_counts[0] > 0
+    "foo"
+  elsif digit_counts[1] > 0
+    "bar"
+  elsif digit_counts[2] > 0
+    "qix"
+  else
+    ""
+  end
 end
 
 def foobarqix(integer)
 
-  digit_check = ""
+  digit_check = digit_check(count_digits(integer))
   divisor_check = check_divisors(integer)
+  puts digit_check
+  puts divisor_check
 
-  threes_in_string, fives_in_string, sevens_in_string = count_digits(integer)
-
-  if (threes_in_string > 0 || fives_in_string > 0 || sevens_in_string > 0) && divisor_check.is_a?(Integer)
-    divisor_check = ""
-  end
-
-  if threes_in_string > 0
-    digit_check = "foo"
-  elsif fives_in_string > 0
-    digit_check = "bar"
-  elsif sevens_in_string > 0
-    digit_check = "qix"
-  else
-    integer
-  end
-
-  if digit_check == ""
-    return divisor_check
+  if (digit_check = "") && (divisor_check=="")
+    return integer
   else
     return divisor_check + digit_check
   end
