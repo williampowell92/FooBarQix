432a0ec79d6355aa8a8705289e0ce08ddae0a249
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-13 15:08:04 +0100

    Incomplete test

--- lib/foobarqix.rb
+++ lib/foobarqix.rb
@@ -1,8 +1,31 @@
 def foobarqix(integer)
+
+  digit_check = ""
+  divisor_check = check_divisors(integer)
+
   threes_in_string = integer.to_s.count("3")
   fives_in_string = integer.to_s.count("5")
   sevens_in_string = integer.to_s.count("7")
 
+  if threes_in_string > 0
+    digit_check = "foo"
+  elsif fives_in_string > 0
+    digit_check = "bar"
+  elsif sevens_in_string > 0
+    digit_check = "qix"
+  else
+    integer
+  end
+
+  if digit_check == ""
+    return divisor_check
+  else
+    return divisor_check + digit_check
+  end
+
+end
+
+def check_divisors(integer)
   if integer % 3 == 0
     "foo"
   elsif integer % 5 == 0
@@ -10,14 +33,6 @@ def foobarqix(integer)
   elsif integer % 7 == 0
     "qix"
   else
-    if threes_in_string > 0
-      "foo"
-    elsif fives_in_string > 0
-      "bar"
-    elsif sevens_in_string > 0
-      "qix"
-    else
-      integer
-    end
+    integer
   end
 end
