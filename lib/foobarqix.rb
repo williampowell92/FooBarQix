b131e6b02fc8a81c6e97bb3c32bd92c22ac31462
William Powell <william.powell92@gmail.com>
2018-04-13 15:21:50 +0100

    Added test for foobarqix(3) == foofoo

--- lib/foobarqix.rb
+++ lib/foobarqix.rb
@@ -1,11 +1,37 @@
+def check_divisors(integer)
+  if integer % 3 == 0
+    "foo"
+  elsif integer % 5 == 0
+    "bar"
+  elsif integer % 7 == 0
+    "qix"
+  else
+    integer
+  end
+end
+
+def count_digits(integer)
+  [
+    integer.to_s.count("3"),
+    integer.to_s.count("5"),
+    integer.to_s.count("7")
+  ]
+end
+
+def digit_check(digit_counts)
+
+end
+
 def foobarqix(integer)
 
   digit_check = ""
   divisor_check = check_divisors(integer)
 
-  threes_in_string = integer.to_s.count("3")
-  fives_in_string = integer.to_s.count("5")
-  sevens_in_string = integer.to_s.count("7")
+  threes_in_string, fives_in_string, sevens_in_string = count_digits(integer)
+
+  if (threes_in_string > 0 || fives_in_string > 0 || sevens_in_string > 0) && divisor_check.is_a?(Integer)
+    divisor_check = ""
+  end
 
   if threes_in_string > 0
     digit_check = "foo"
@@ -23,16 +49,4 @@ def foobarqix(integer)
     return divisor_check + digit_check
   end
 
-end
-
-def check_divisors(integer)
-  if integer % 3 == 0
-    "foo"
-  elsif integer % 5 == 0
-    "bar"
-  elsif integer % 7 == 0
-    "qix"
-  else
-    integer
-  end
 end
